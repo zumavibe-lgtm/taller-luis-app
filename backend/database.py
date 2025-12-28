20954b6970a04c1320b1eb4c02477792a4002bbf
arturo <zumavibe@gmail.com>
2025-12-28 09:38:43 -0600

    Preparando base de datos para la nube

--- backend/database.py
+++ backend/database.py
@@ -1,24 +1,23 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-# ⚠️ OJO: CAMBIA 'tu_contraseña' POR LA CONTRASEÑA REAL QUE USASTE EN PGADMIN
-# Formato: postgresql://usuario:contraseña@servidor:puerto/nombre_base_datos
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:nomames@localhost:5432/taller_db"
+# 1. Buscamos si hay una base de datos en la nube (Variable de Entorno)
+# Si no hay, usamos la local (sqlite)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Crear el motor de conexión
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    # Fix pequeño para Render: ellos usan postgres:// pero SQLAlchemy quiere postgresql://
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-# Crear la sesión (es lo que usaremos para guardar/leer datos)
+if not DATABASE_URL:
+    # MODO LOCAL
+    DATABASE_URL = "sqlite:///./taller.db"
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    # MODO NUBE (Postgres)
+    engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base para los modelos
 Base = declarative_base()
-
-# Función para obtener la base de datos en cada petición
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
